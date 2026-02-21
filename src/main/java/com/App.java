1462c53be960d2bd5aaa3b3f4b80ead8968e9094
shraddhapal27 <palshraddha41@gmail.com>
2026-02-21 21:47:01 +0530

    [SHraddha Pal] : Added feature 1 Feet equality program

---
+++ src/main/java/com/App.java
@@ -0,0 +1,12 @@
+package com;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        System.out.println("Are equal? " + f1.equals(f2));
+    }
+}

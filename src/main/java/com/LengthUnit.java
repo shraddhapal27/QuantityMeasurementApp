4724d3a0fbcd9ba92b502539f035362b3bf67891
shraddhapal27 <palshraddha41@gmail.com>
2026-02-23 12:48:08 +0530

    [Shraddha Pal] : Added feature 6 program

--- src/main/java/com/LengthUnit.java
+++ src/main/java/com/LengthUnit.java
@@ -1,8 +1,10 @@
+
 package com;
 
 public enum LengthUnit {
 
 	FEET(12.0), INCHES(1.0), YARDS(36.0), CENTIMETERS(0.393701);
+
 	private final double conversionFactor;
 
 	LengthUnit(double conversionFactor) {
